67f69df19452033917ce7af98ad880ba22457a00
Dhayu Intan Nareswari <91419607+DhayuIntan@users.noreply.github.com>
2023-12-11 11:36:38 +0700

    W15: Edit Profile

--- src/tap_scan/lib/pages/profile_page.dart
+++ src/tap_scan/lib/pages/profile_page.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tap_scan/components/components.dart';
-import 'package:tap_scan/pages/login_page.dart';
 import 'package:tap_scan/pages/my_scans_page.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -12,48 +11,132 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          body: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 25,
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              const Text(
+                "Profile",
+                style: TextStyle(
+                  color: Color.fromRGBO(0, 198, 232, 1),
+                  fontWeight: FontWeight.w700,
+                  fontSize: 21,
                 ),
-                Text(
-                  "Profile",
-                  style: TextStyle(
-                    color: Color.fromRGBO(0, 198, 232, 1),
-                    fontWeight: FontWeight.w700,
-                    fontSize: 21,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                BackButton(),
-                SizedBox(
-                  height: 40,
-                ),
-                ProfilePic(),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const BackButton(),
+              const SizedBox(
+                height: 40,
+              ),
+              const ProfilePic(),
+              const SizedBox(
+                height: 20,
+              ),
+              InkWell(
+                // Wrap the "Edit Profile" text with InkWell for tap interaction
+                onTap: () {
+                  // Navigate to the edit profile page when tapped
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          const EditProfilePage(), // Create EditProfilePage class
+                    ),
+                  );
+                },
+                child: const Text(
                   "Edit Profile",
                   style: TextStyle(
                     color: Color.fromRGBO(0, 198, 232, 1),
                   ),
                 ),
-                ProfileDatas()
-              ],
-            ),
+              ),
+              const ProfileDatas(),
+            ],
           ),
-          floatingActionButton: const MainFloatingActionButton(),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          bottomNavigationBar: const MainBottomNavBar(
-            index: 1,
-          )),
+        ),
+        floatingActionButton: const MainFloatingActionButton(),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: const MainBottomNavBar(
+          index: 1,
+        ),
+      ),
+    );
+  }
+}
+
+class EditProfilePage extends StatefulWidget {
+  const EditProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Profile"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Edit Email:"),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                hintText: "Enter your email",
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text("Edit Name:"),
+            TextField(
+              controller: _nameController,
+              decoration: const InputDecoration(
+                hintText: "Enter your name",
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Text("Edit Phone Number:"),
+            TextField(
+              controller: _phoneNumberController,
+              decoration: const InputDecoration(
+                hintText: "Enter your phone number",
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {});
+                FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(currentUser!.uid)
+                    .set({
+                  'username': _nameController.text.trim(),
+                  'email': _emailController.text.trim(),
+                  'phoneNumber': _phoneNumberController.text.trim(),
+                });
+                Navigator.pop(context);
+                // Save the edited data to Firestore or perform necessary actions
+                // For simplicity, just print the edited data for now
+              },
+              child: const Text("Save"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -74,7 +157,7 @@ class ProfileDatas extends StatelessWidget {
           .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
-          return CircularProgressIndicator(); // Menampilkan indikator loading jika data belum tersedia
+          return const CircularProgressIndicator(); // Menampilkan indikator loading jika data belum tersedia
         }
 
         if (snapshot.hasError) {
